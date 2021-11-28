b0c0d88d889bcec812f1b2358d1d0d20611080dd
flansuru <91861474+flansuru@users.noreply.github.com>
2021-11-27 22:10:53 -0400

    Update GoogleProvider.js

--- src/classes/tts/providers/GoogleProvider.js
+++ src/classes/tts/providers/GoogleProvider.js
@@ -10,7 +10,7 @@ const languages = require('../../../../data/languages.json');
 class GoogleProvider extends AbstractProvider {
   constructor() {
     super();
-    this.lang = 'es';
+    this.lang = 'it';
     this.slow = false;
   }
 
